38e7d5d52cfd3a3752d8b22fe58698ab1799121e
AngusWu <anguswu@simbo.com.tw>
2018-12-01 20:11:17 +0800

    pass level 4

--- Player.js
+++ Player.js
@@ -1,7 +1,8 @@
 class Player {
 
   constructor(){
-    this.hp_max = 20;
+    this.hp_max = 12;
+    this.hp = this.hp_max;
   }
 
 
@@ -10,20 +11,31 @@ class Player {
     const space = warrior.feel();
 
     if(this.shouldAttack(space)){
+
       warrior.attack();
-      return;
+
+    }else {
+
+      if(this.shouldRest(warrior)){
+
+        warrior.rest();
+        
+      }else {
+
+        warrior.walk();
+
+      }
     }
 
-    if(this.shouldRest(warrior)){
-      warrior.rest();
-      return;
-    }
-
-    warrior.walk();
+    this.hp = warrior.health();
   }
 
   shouldRest(warrior) {
-    return (this.hp_max - warrior.health()) >= 14;
+    return this.hp_max > warrior.health() && !this.isTakingDamage(warrior);
+  }
+
+  isTakingDamage(warrior) {
+    return this.hp > warrior.health();    
   }
 
   shouldAttack(space) {
